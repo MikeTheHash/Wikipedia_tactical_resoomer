ddba71bae89d72830ee670247c04ee21160bc34e
Mike <80423783+MikeTheHash@users.noreply.github.com>
2022-04-04 15:05:13 -0400

    Update Linux_installer.sh

--- Linux_installer.sh
+++ Linux_installer.sh
@@ -1,6 +1,10 @@
+echo "Installing Figlet..."
 sudo apt-get install figlet
 
+echo "Installing external libraries"
 pip install wikipedia
 pip install colorama
 pip install translate
 pip install os
+
+echo "Operations completed"
